5bf05c7a7902b99312c9f84d5ad1863fe4496b37
patel sapna <2302031030082@silveroakuni.ac.in>
2025-06-27 10:47:04 +0530

    Add files via upload

---
+++ firstreact/src/App.js
@@ -0,0 +1,50 @@
+import Class_component from "./Component/Class_component"
+import Function_component from "./Component/Function_component"
+import Jsx from "./JSX/Jsx"
+import Main_layout from "./Layout/Main_layout"
+import Main_props from "./Props/Main_props"
+import Routing from "./Routing/Routing"
+import Route_app from "./new routing/Route_app"
+import Sass_css from "./Sass_css/Sass_css";
+
+import Main_classstate from "./state/Class/Main_classstate";
+import Main_funcstate from "./state/Func/Main_funcstate";
+
+import Module_css from "./Module_css/Module_css";
+
+
+
+function App() {
+  return (
+    <div>
+      {
+        //<Function_component/>
+        //<Class_component/>
+
+        //<Jsx/>
+        //<Main_layout/>
+
+        //<Module_css/>
+
+        //<Sass_css/>
+
+          <Main_funcstate/>
+       // <Main_classstate/>
+
+
+        
+        //  <Main_props/>
+
+      // <Routing/>
+      //<Route_app/>
+    
+
+
+
+       
+
+      }
+    </div>
+  )
+}
+export default App
