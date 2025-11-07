959b723f10ed4b99b6467ccf2e49485a06e51ba5
patel sapna <2302031030082@silveroakuni.ac.in>
2025-11-07 16:46:01 +0530

    Add files via upload

---
+++ bridalmekup/src/website/component/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        
+            <footer>
+                <div className="container">
+                    <div className="col-lg-8">
+                        <p>Copyright © 2025 Kishori Co., Ltd. All rights reserved.
+                            Design: <a rel="nofollow" href="https://templatemo.com" target="_blank">Sapna</a></p>
+                    </div>
+                </div>
+            </footer>
+       
+
+    )
+}
+
+export default Footer
